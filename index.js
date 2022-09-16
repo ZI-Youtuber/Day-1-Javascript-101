5fac55ba1230ff26f12e023a0016137bd4d19356
Zoraez <82158378+ZI-Youtuber@users.noreply.github.com>
2022-09-16 16:20:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log('Hello World')
+
+console.log('Zoraez')
+
+// Variable
+
+name = 'Brookie'
+
+console.log(name)
+
+greeting = 'Hello I am someone'
+
+console.log(greeting)
+
+// Operators
+// Score Caculator
+
+score1 = Number(prompt('What is your score ?'))
+score2 = Number(prompt('What is your 2nd score ?'))
+difference = score1 - score2
+total = score1 + score2
+
+console.log('Difference of Scores: ' , difference)
+console.log('Total of Scores: ' , total)
